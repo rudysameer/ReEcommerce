2c284a8883c1681e6b5c92ea75cfc68cb37aeecb
rudysameer <matinamaicha23@gmail.com>
2024-08-06 22:51:08 +0545

    models views commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200,unique=True)
+    logo = models.CharField(max_length=200)
+    slug = models.CharField(max_length=300,unique=True)
+    def __str__(self):
+        return self.name
+
+class Slider(models.Model):
+    name = models.CharField(max_length=300,unique=True)
+    image = models.ImageField(upload_to='media')
+    rank = models.IntegerField()
+    description = models.TextField(blank=True)
+    url = models.URLField(max_length=500,blank=True)
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length=200)
+    desciption = models.TextField(blank=True)
+    image = models.ImageField(upload_to='media')
+    rank = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200,unique=True)
+    image = models.ImageField(upload_to='media')
+    slug = models.CharField(max_length=300,unique=True)
+    def __str__(self):
+        return self.name
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=200)
+    profession = models.CharField(max_length=100)
+    star = models.IntegerField()
+    image = models.ImageField(upload_to='media')
+    comment = models.TextField(blank=True)
+    def __str__(self):
+        return self.name
+
--- home/views.py
+++ home/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import *
 # Create your views here.
 class BaseView(View):
     view = {}
+    view['categories'] = Category.objects.all()
+    view['brands'] = Brand.objects.all()
+
 
 class HomeView(BaseView):
     def get(self,request):
-        return render(request,'index.html')
+        self.view
+        self.view['sliders'] = Slider.objects.all()
+        self.view['ads'] = Ad.objects.all()
+
+        return render(request,'index.html',self.view)
 
 
