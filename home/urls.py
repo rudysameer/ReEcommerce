3984c71004055e327d576be3d8778139aa773b9c
rudysameer <matinamaicha23@gmail.com>
2024-09-09 15:35:28 +0545

    Sign up

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from .views import *
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
     path("category/<slug>", CategoryView.as_view(), name='category'),
-    path("brand/<slug>",BrandView.as_view(), name='brand')
-
+    path("brand/<slug>", BrandView.as_view(), name='brand'),
+    path("product/<slug>", ProductDetailView.as_view(), name='productDetail'),
+    path("search", SearchView.as_view(), name="search"),
+    path("signup", signup, name="signup"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views.generic import View
 from .models import *
+from django.db.models import Q
 
 
 # Create your views here.
@@ -36,7 +39,82 @@ class CategoryView(BaseView):
 class BrandView(BaseView):
     def get(self, request, slug):
         brand = Brand.objects.get(slug=slug)
-        self.view['bra'] = brand
-        self.view['brand_products'] = Product.objects.filter(brand = brand)
+        self.view['brand'] = brand
+        self.view['brand_products'] = Product.objects.filter(brand=brand)
 
-        return render(request,'brand.html', self.view)
+        return render(request, 'brand.html', self.view)
+
+
+class ProductDetailView(BaseView):
+    def get(self, request, slug):
+
+        # self.view['product_details'] = Product.objects.filter(slug=slug)
+        # product_detail = Product.objects.get(slug=slug)
+        product_detail = get_object_or_404(Product, slug=slug)
+        self.view['product_details'] = product_detail
+        related_brands = product_detail.brand
+        self.view['related_brands'] = Product.objects.filter(brand = related_brands).exclude(slug = slug)
+        related_label = product_detail.label
+        self.view['related_label'] = Product.objects.filter(label = related_label).exclude(slug = slug)
+
+
+        return render(request,'product-detail.html',self.view)
+
+
+class SearchView(BaseView):
+    def get(self,request):
+        self.view
+        # if request.method == 'POST':
+        #     query = request.GET('search')
+        query = request.GET.get('search')
+        if query != '':
+            self.view['search_products'] = Product.objects.filter(
+                Q(product_name__icontains = query)| Q(description__icontains = query)
+            )
+
+        elif query == ' ':
+            return redirect('/')
+
+        else:
+            return redirect('/')
+
+
+
+        return render(request,'search.html',self.view)
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+
+        if password == cpassword:
+            if User.objects.filter(username = username).exists():
+                messages.error(request,"Username already exists. try new")
+                return redirect("/signup")
+            elif User.objects.filter(email = email).exists():
+                messages.error(request,"Email is already occupied. try new")
+
+            else:
+                User.objects.create_user(
+                    username = username,
+                    email = email,
+                    first_name = first_name,
+                    last_name = last_name,
+                    password=password
+                )
+                messages.success(request,"User successfully created")
+                return redirect("/")
+
+        else:
+            messages.error(request,"Passwords donot match")
+            return redirect("/signup")
+
+    return render(request,'signup.html')
+
+
+
